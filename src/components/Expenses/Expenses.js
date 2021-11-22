cbf0020254050c1fe2a35bb07163b6d5f9af79c3
John <john.long@activeops.com>
2021-11-22 15:26:51 +0000

    Implemented active filtering by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,11 +13,15 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeYearFilter={yearFilterChangedHandler}/>
-        {props.items.map(expense => 
+        {filteredExpenses.map(expense => 
         <ExpenseItem
           key={expense.id}
           title={expense.title}
